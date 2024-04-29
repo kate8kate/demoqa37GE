730e3923a55649328c269b28667b443751819a58
kate8kate <kate.bazire@gmail.com>
2024-04-30 00:57:32 +0200

    Initial commit

---
+++ src/main/java/config/ApplicationManager.java
@@ -0,0 +1,59 @@
+package config;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.time.Duration;
+
+public class ApplicationManager {
+
+    static WebDriver driver;
+    String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
+    public void init() {
+        if(browser.equals("chrome")) {
+            // with tools:
+            //driver = new ChromeDriver();
+            // with WebDriverManager
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--lang=en");
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver(chromeOptions);
+        } else if (browser.equals("firefox")) {
+            // driver = new FirefoxDriver();
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver(firefoxOptions);
+        } else if (browser.equals("edge")) {
+            //driver  = new EdgeDriver();
+            EdgeOptions edgeOptions = new EdgeOptions();
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver(edgeOptions);
+        } else if (browser != null && browser != "chrome"
+                && browser != "firefox" && browser != "edge") {
+            throw new IllegalArgumentException("browser entered not correct");
+        }
+
+        // common settings for the browser
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.navigate().to("https://demoqa.com/");
+
+    }
+
+    public void quit() {
+        driver.quit();
+    }
+
+}
